6788dcdb93dbd2152c98c9fecfbab4398297a0e2
Nicolas Berard <nicolas.berard@canaltp.fr>
2016-09-28 16:39:08 +0200

    watch symfony logs for prod env

--- docker/run.sh
+++ docker/run.sh
@@ -4,7 +4,7 @@ php /opt/composer.phar install -n -q
 IFS=', ' read -r -a BOOK_DAYS <<< "$BOOK_DAYS"
 for DAY in "${BOOK_DAYS[@]}"; do
     DAY_OF_WEEK=$(date -d$DAY +%u)
-    LINE="1 0 * * $DAY_OF_WEEK /usr/local/bin/php /opt/wanaplay-bot/app/console wanaplay:wanaplay_bot:book $BOOK_TIME >> /var/log/cron.log 2>&1"
+    LINE="1 0 * * $DAY_OF_WEEK /usr/local/bin/php /opt/wanaplay-bot/app/console wanaplay:wanaplay_bot:book $BOOK_TIME -e prod >> /var/log/cron.log 2>&1"
     echo "$LINE" >> $CRONTAB_PATH
 done
 echo "" >> $CRONTAB_PATH
@@ -13,6 +13,7 @@ echo "Crontab created:"
 cat $CRONTAB_PATH
 
 touch /var/log/cron.log
+touch /opt/wanaplay-bot/app/logs/prod.log
 crontab -u root $CRONTAB_PATH
 cron -f &
-tail -f /var/log/cron.log
+tail -f /var/log/cron.log /opt/wanaplay-bot/app/logs/prod.log
